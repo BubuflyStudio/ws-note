9fc7ed6922920d9a03ca6bdd86b5cca9dda744e5
wujohns <wujohns@163.com>
2017-11-28 17:41:12 +0800

    增加engine.io 相关实验

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * 针对 engine.io 或 socket.io 中对应客户端的编译
+ *
+ * @author wujohns
+ * @date 17/11/28
+ */
+'use strict';
+
--- scripts/engine_io/server.js
+++ scripts/engine_io/server.js
@@ -1,8 +1,15 @@
 /**
- * engine.io 服务端代码
+ * engine.io 使用案例
  *
  * @author wujohns
  * @date 17/11/28
  */
 'use strict';
 
+// const engine = require('engine.io');
+const engine = require('../../learn/engine_server/engine.io.js');
+
+const server = engine.listen(80);
+server.on('connection', (socket) => {
+    socket.send('测试');
+});
